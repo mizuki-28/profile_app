06167720c01fdc8e15a50005cc70d3b7507a5b99
Mizuki <104258415+mizuki-28@users.noreply.github.com>
2023-01-25 15:59:14 +0900

    seedファイル 修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,84 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create!(
+  email: 'user1@example.com',
+  name: 'ユーザー',
+  image: File.open('./app/assets/images/avatar.png')
+)
+
+Category.create!(
+  [
+    {
+      category_name: 'バックエンド'
+    },
+    {
+      category_name: 'フロントエンド'
+    },
+    {
+      category_name: 'インフラ'
+    }
+  ]
+)
+
+Skill.create!(
+  [
+    {
+      skill_name: 'Ruby',
+      level: 40,
+      user_id: 1,
+      category_id: 1
+    },
+    {
+      skill_name: 'Rails',
+      level: 40,
+      user_id: 1,
+      category_id: 1
+    },
+    {
+      skill_name: 'MySQL',
+      level: 40,
+      user_id: 1,
+      category_id: 1
+    },
+    {
+      skill_name: '削除用',
+      level: 100,
+      user_id: 1,
+      category_id: 1
+    },
+    {
+      skill_name: 'HTML',
+      level: 40,
+      user_id: 1,
+      category_id: 2
+    },
+    {
+      skill_name: 'CSS',
+      level: 40,
+      user_id: 1,
+      category_id: 2
+    },
+    {
+      skill_name: 'JavaScript',
+      level: 40,
+      user_id: 1,
+      category_id: 2
+    },
+    {
+      skill_name: 'Heroku',
+      level: 40,
+      user_id: 1,
+      category_id: 3
+    },
+    {
+      skill_name: 'AWS',
+      level: 40,
+      user_id: 1,
+      category_id: 3
+    },
+    {
+      skill_name: 'Firebase',
+      level: 40,
+      user_id: 1,
+      category_id: 3
+    }
+  ]
+)
